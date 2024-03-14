7d22e3ada0e09345fcab54032c129b16a2b09450
Moctor42 <moctor42@Ubuntu.myguest.virtualbox.org>
2024-03-14 14:21:14 +0000

    articles can be listed by topic

--- src/components/pages/Articles.jsx
+++ src/components/pages/Articles.jsx
@@ -1,42 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { Card } from '../Card';
-import { Link } from 'react-router-dom';
-import { fetchArticles } from '../../../utils/utils';
+import React, { useEffect, useState } from 'react'
+import { Card } from '../Card'
+import { Link, useSearchParams } from 'react-router-dom'
+import { fetchArticles } from '../../../utils/utils'
+import { TopicDropdown } from '../TopicDropdown'
 
 export const Articles = () => {
-    const [fetchedArticles, setFetchedArticles] = useState("")
+    const [fetchedArticles, setFetchedArticles] = useState('')
     const [isLoading, setIsLoading] = useState(true)
+    const [searchParams, setSearchParams] = useSearchParams()
 
-    useEffect(()=>{
-        fetchArticles()
-        .then((response)=>{
-            setTimeout(()=>{
+    const topicQuery = searchParams.get('topic')
+
+    useEffect(() => {
+        setIsLoading(true)
+        let query = "?"
+
+        if (topicQuery) {
+            searchParams.set('topic', topicQuery)
+            setSearchParams(searchParams)
+            query += `&topic=${topicQuery}`
+        } else {setSearchParams()}
+
+        fetchArticles(query).then((response) => {
+            setTimeout(() => {
                 setFetchedArticles(response)
                 setIsLoading(false)
             }, 1000)
-            
         })
-    }, [])
+    }, [searchParams])
 
-    const makeList = (fetchedData)=>{
-        if(!fetchedData) setIsLoading(true)
-        return fetchedData.map((cardInfo)=>
-        <Link to={`/articles/${cardInfo.article_id}`} key={cardInfo.article_id}> 
-            <li> <Card info={cardInfo}/> </li>
-        </Link>
-        )
+    const makeList = (fetchedData) => {
+        if (!fetchedData) setIsLoading(true)
+        return fetchedData.map((cardInfo) => (
+            <Link to={`/articles/${cardInfo.article_id}`} key={cardInfo.article_id}>
+                <li>
+                    {' '}
+                    <Card info={cardInfo} />{' '}
+                </li>
+            </Link>
+        ))
     }
 
     return (
-        <div className='page page-style'>
-            <h2>Articles</h2>
-            {isLoading ? 
-            <h2 className='loading'>loading...</h2> : 
-                <div className='scrollcontainer'>
+        <div className="page page-style">
+            <div>
+                <h2>Articles</h2> <TopicDropdown searchParams={searchParams} setSearchParams={setSearchParams} topicQuery={topicQuery} />{' '}
+            </div>
+            {isLoading ? (
+                <h2 className="loading">loading...</h2>
+            ) : (
+                <div className="scrollcontainer">
                     <ul> {makeList(fetchedArticles)} </ul>
                 </div>
-                }
-            
+            )}
         </div>
-    );
-};
+    )
+}
--- utils/utils.js
+++ utils/utils.js
@@ -4,46 +4,57 @@ export const fetchSingleArticle = (articleId) => {
     return axios
         .get(`https://nc-news-8n39.onrender.com/api/articles/${articleId}`)
         .then((response) => response.data.article)
-        .catch((err) => console.log(err, 'fetchSingleArticle error!'))
+        .catch((err) => console.log(err, 'fetchSingleArticle axios error!'))
 }
 
-export const fetchArticles = () => {
+export const fetchArticles = (query) => {
+
+    let url = `https://nc-news-8n39.onrender.com/api/articles`
+    if(query) url += query
+
     return axios
-        .get(`https://nc-news-8n39.onrender.com/api/articles`)
+        .get(url)
         .then((response) => response.data.articles)
-        .catch((err) => console.log(err, 'fetchArticles error!'))
+        .catch((err) => console.log(err, 'fetchArticles axios error!'))
 }
 
 export const fetchComments = (articleId) => {
     return axios
         .get(`https://nc-news-8n39.onrender.com/api/articles/${articleId}/comments`)
         .then((response) => response.data.comments)
-        .catch((err) => console.log(err, 'fetchComments error!'))
+        .catch((err) => console.log(err, 'fetchComments axios error!'))
 }
 
 export const patchArticle = (articleId, body) => {
     return axios
         .patch(`https://nc-news-8n39.onrender.com/api/articles/${articleId}`, body)
-        .catch((err) => console.log(err, 'patchArticle error!'))
+        .catch((err) => console.log(err, 'patchArticle axios error!'))
 }
 
 export const postComment = (articleId, body)=>{
     return axios
         .post(`https://nc-news-8n39.onrender.com/api/articles/${articleId}/comments`, body)
-        .catch((err) => console.log(err.response.data, 'postComment error!'))
+        .catch((err) => console.log(err.response.data, 'postComment axios error!'))
 }
 
 export const deleteComment = (commentId)=>{
     return axios
     .delete(`https://nc-news-8n39.onrender.com/api/comments/${commentId}`)
-    .catch((err)=> console.log(err, "deleteComment error!"))
+    .catch((err)=> console.log(err, "deleteComment axios error!"))
 }
 
 export const fetchUsers = () => {
     return axios
     .get('https://nc-news-8n39.onrender.com/api/users/')
     .then(response => response.data.users)
-    .catch(err => console.log(err, 'fetchUsers error!'))
+    .catch(err => console.log(err, 'fetchUsers axios error!'))
+}
+
+export const fetchTopics = ()=>{
+    return axios
+    .get('https://nc-news-8n39.onrender.com/api/topics')
+    .then(response => response.data.topics.rows)
+    .catch(err => console.log(err, 'fetchTopics axios error!'))
 }
 
 export const formatDate = (timestamp) => {
