66efec50592fd11d61f413ce7e6d617a440d1d69
Moctor42 <moctor42@Ubuntu.myguest.virtualbox.org>
2024-03-15 13:53:45 +0000

    edited error page

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import imgUrl from '../../assets/whoops.png'
 
 export const Error = () => {
     return (
         <div className="page-style">
             <h2 className="page-header">There's nothing here...</h2>
-            <img src="/src/assets/whoops.png" alt="a man looking at a missing server" title="server must be out for lunch" />
+            <img src={imgUrl} alt="a man looking at a missing server" title="server must be out for lunch" />
         </div>
     )
 }
